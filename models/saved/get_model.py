8dde981a0895697151b9a636528fe73cb1a06da4
Najks <nik.damis@gmail.com>
2026-03-11 11:21:09 +0100

    Moved get_model to the correct location.

--- models/saved/get_model.py
+++ models/saved/get_model.py
@@ -3,7 +3,7 @@ from huggingface_hub import hf_hub_download
 model_path = hf_hub_download(
     repo_id="Najks/AutoAM",
     filename="best.pt",
-    local_dir="."
+    local_dir="../.."
 )
 
 print(f"Model downloaded to: {model_path}")
